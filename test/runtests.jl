49f64becf9d32ac708a376cd9c9578b2168f69d2
Wojciech Stark <66268167+wgst@users.noreply.github.com>
2023-06-06 15:35:30 +0100

    Reverse ACEds test - ACEds has to be registered first

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,3 @@ using SafeTestsets
 @safetestset "Cube" begin include("test_cube.jl") end
 @safetestset "CubeDensity" begin include("test_cube_density.jl") end
 @safetestset "SciKitDensity" begin include("test_scikit_density.jl") end
-@safetestset "ACEdsFrictionTensor" begin include("test_aceds_odf.jl") end
