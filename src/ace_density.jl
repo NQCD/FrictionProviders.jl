acd8806f3fd7ca760eaa726aa87f75785b156992
Wojciech Stark <66268167+wgst@users.noreply.github.com>
2023-09-12 14:06:28 +0100

    Update ace_density.jl

--- src/ace_density.jl
+++ src/ace_density.jl
@@ -17,7 +17,8 @@ end
 
 function density!(model::ACEDensity, rho::AbstractVector, R::AbstractMatrix, friction_atoms::AbstractVector)
     for i in friction_atoms
-        xR = R[:,[1:end-2,i]]
+        xR = R[:,1:end-length(friction_atoms)+1]
+        xR[:,end]=R[:,i]
         rho[i] = austrip(NQCModels.potential(model.ml_model, xR) * model.density_unit)
     end
 end
