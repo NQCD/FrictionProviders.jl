373ccc9038bbf421459d1e50c7b83e5698151482
Alexander Spears <alexander.spears@warwick.ac.uk>
2024-10-17 10:53:31 +0100

    resolved method ambiguity in cube test

--- test/test_cube.jl
+++ test/test_cube.jl
@@ -4,12 +4,12 @@ ENV["JULIA_PYTHONCALL_EXE"] = "@PyCall"  # optional
 using FrictionProviders
 using Test
 using NQCBase
-using PythonCall
-using PyCall: pyimport
+using PyCall
 
-
-sys = pyimport("sys")
+py"""
+import sys
 sys.path.insert(0, ".")
+"""
 
 cube = pyimport("cube")
 
@@ -25,16 +25,15 @@ cube_object.read(filename)
     r = c.origin .+ 1e-8
     @test c(r) ≈ cube_object(au_to_ang.(r)...)
 
-    for _=1:10
+    for _ = 1:10
         r = c.origin
-        r += rand() * c.cell.vectors[:,1]
-        r += rand() * c.cell.vectors[:,2]
-        r += rand() * c.cell.vectors[:,3]
+        r += rand() * c.cell.vectors[:, 1]
+        r += rand() * c.cell.vectors[:, 2]
+        r += rand() * c.cell.vectors[:, 3]
 
         @test c(r) ≈ cube_object(au_to_ang.(r)...)
     end
 
-    r = c.origin + c.cell.vectors[:,1] + c.cell.vectors[:,2] + c.cell.vectors[:,3] .- 1e-8
+    r = c.origin + c.cell.vectors[:, 1] + c.cell.vectors[:, 2] + c.cell.vectors[:, 3] .- 1e-8
     @test c(r) ≈ cube_object(au_to_ang.(r)...)
 end
-
